fe11c6d94a66eaa4d0b41b180685ceb6aea444c4
Jenny Paola <jennyk_94@hotmail.com>
2017-11-17 23:20:15 -0500

    Complemento Taller 4 V4.1

---
+++ build/web/js/Modelo.js
@@ -0,0 +1,41 @@
+var idFacultad=3;
+var facultades=[
+   
+
+];
+var idCarrera=3;
+var carreras=[
+    
+];
+
+var idMunicipio=4;
+var municipios=[
+    {"id":1, "nombre":"Tunja"},
+    {"id":2, "nombre":"Duitama"},
+    {"id":3, "nombre":"Sogamoso"},
+];
+
+var idEstudiantes=3;
+var estudiantes=[
+    
+];
+
+var idMateria=3;
+var materia=[
+    
+];
+
+var idProfesor=10;
+var Profesor=[
+    
+];
+
+var idHorario=20;
+var horario=[
+    
+];
+
+var idMatricula=20;
+var matricula=[
+    
+];
